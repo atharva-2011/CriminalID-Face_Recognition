52043f3cf8b0b380ee5387274985e3dbcb829c38
atharva-2011 <82864401+atharva-2011@users.noreply.github.com>
2026-03-16 13:33:17 +0530

    Fix model loading function syntax

--- CriminalID.py
+++ CriminalID.py
@@ -302,7 +302,7 @@ def warmup_model(model):
 
 @st.cache_resource
 def load_model(path):
-    model = return tf.keras.models.load_model(path,compile=False)
+    model = tf.keras.models.load_model(path,compile=False)
     return model
 
 @st.cache_data
